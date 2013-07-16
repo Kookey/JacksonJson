973d0a050e96daf55b16648d9aa047d95896e7e6
Kookey <wxlhdm@qq.com>
2013-07-16 18:09:48 +0800

    JacksonTest.java新增Map和List转成json

--- src/com/steel/entity/JacksonTest.java
+++ src/com/steel/entity/JacksonTest.java
@@ -1,6 +1,10 @@
 package com.steel.entity;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,10 +12,12 @@ import org.junit.Test;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+
 /**
  * 
  * @author admin
- *
+ * 
  */
 public class JacksonTest {
 
@@ -29,7 +35,8 @@ public class JacksonTest {
 		bean.setName("wangxl");
 		objectMapper = new ObjectMapper();
 		try {
-			jsonGenerator = objectMapper.getJsonFactory().createGenerator(System.out, JsonEncoding.UTF8);
+			jsonGenerator = objectMapper.getJsonFactory().createGenerator(
+					System.out, JsonEncoding.UTF8);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -51,12 +58,12 @@ public class JacksonTest {
 		} catch (Exception e) {
 		}
 	}
-	
+
 	/**
 	 * Java对象转成json
 	 */
-	@Test
-	public void writeEntityJson(){
+	//@Test
+	public void writeEntityJson() {
 		try {
 			System.out.println("jsonGenerator");
 			jsonGenerator.writeObject(bean);
@@ -64,6 +71,92 @@ public class JacksonTest {
 			System.out.println("ObjectMapper");
 			objectMapper.writeValue(System.out, bean);
 		} catch (Exception e) {
+		}
+	}
+	/**
+	 * 将Map集合转成Json字符串
+	 */
+	//@Test
+	public void writeMapJson() {
+		try {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("name", bean.getName());
+			map.put("account", bean);
+			bean = new AccountBean();
+			bean.setAddress("china-beijing");
+			bean.setEmai("wxlhdm@163.com");
+			map.put("account2", bean);
+			System.out.println("jsonGenerator");
+			jsonGenerator.writeObject(map);
+			System.out.println("");
+			System.out.println("objectMapper");
+			objectMapper.writeValue(System.out, map);
+		} catch (Exception e) {
+		}
+	}
+	
+	/**
+	 * 将List集合转成json
+	 */
+	@Test
+	public void writeListJSON(){
+		List<AccountBean> list = new ArrayList<AccountBean>();
+		list.add(bean);
+		bean = new AccountBean();
+		bean.setId(2);
+		bean.setAddress("address2");
+		bean.setEmai("email2");
+		bean.setName("houdm");
+		list.add(bean);
+		
+		System.out.println("jsonGenerator");
+		try {
+			//list转成JSON字符串
+			jsonGenerator.writeObject(list);
+			System.out.println();
+			System.out.println("objectMappper");
+			//用objectMapper直接返回list转成的JSON字符串
+			System.out.println("1、" + objectMapper.writeValueAsString(list));
+			System.out.println("2、");
+			//objectMapper list转成JSON字符串
+			objectMapper.writeValue(System.out, list);
+		} catch (Exception e) {
+		}
+	}
+	
+	public void writeOthersJSON() {
+		String[] arr = {"a" ,"b" ,"c"};
+		System.out.println("jsonGenerator");
+		String str = "hello world jackson";
+		try {
+			//byte
+			jsonGenerator.writeBinary(str.getBytes());
+			//boolean
+			jsonGenerator.writeBoolean(true);
+			//null
+			jsonGenerator.writeNull();
+			//float
+			jsonGenerator.writeNumber(2.2f);
+			//char
+			jsonGenerator.writeRaw("c");
+			//String
+			jsonGenerator.writeRaw(str, 5, 10);
+			//String
+			jsonGenerator.writeRawValue(str, 5, 5);
+			//String
+			jsonGenerator.writeString(str);
+			
+			jsonGenerator.writeTree(JsonNodeFactory.instance.POJONode(str));
+			System.out.println();
+			
+			//Object
+			jsonGenerator.writeStartObject();
+			jsonGenerator.writeObjectFieldStart("user");
+			jsonGenerator.writeStringField("name", "jackson");
+			jsonGenerator.writeBooleanField("sex", true);
+			jsonGenerator.writeNumberField("age", 22);
+		} catch (Exception e) {
+			
 		}
 	}
 }
